fb9a3ad1d6fd121d30e4fc18c39173b5438eae65
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-04-07 16:27:20 +0530

    Add files via upload

---
+++ Viral Advertising.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    shared=5
+    i=2
+    liked=shared//2
+    cumulative=liked
+    while(i<=n):
+        shared=liked*3
+        liked=shared//2
+        cumulative+=liked
+
+
+        i+=1
+    return cumulative
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
