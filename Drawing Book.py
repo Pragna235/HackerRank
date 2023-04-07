b7e294b931fe50d20cbf45a0e0c17557416a1548
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-04-07 15:16:52 +0530

    Add files via upload

---
+++ Drawing Book.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    # Write your code here
+    p1=p//2
+    t1=n//2
+    front=p1
+    back=t1-p1
+    return min(front,back)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
