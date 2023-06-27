c59b3ebd8802c09a690b35e441a802be1981fa0b
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-06-27 15:38:13 +0530

    Create Left Rotation.cpp

---
+++ Left Rotation.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n,d;
+    cin>>n>>d;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    d=d%n;
+    for(int i=d;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    for(int i=0;i<d;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
